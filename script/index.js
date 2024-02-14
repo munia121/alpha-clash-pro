fbee91e9a976e3b4e1cc037051a25d1e0236eed1
unknown <muniyaakter21@gmail.com>
2024-02-14 16:45:23 +0600

    add

--- script/index.js
+++ script/index.js
@@ -12,9 +12,26 @@
 // }
 
 
+function continueGame(){
+    // set-1: generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    console.log('your random alphabet',alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const cureentAlphabet = document.getElementById('cureent-alphabet')
+    cureentAlphabet.innerText = alphabet;
+
+
+    // set background color key bod 
+    setBackgroundColorById(alphabet);
+
+
+}
+
       
 
 function play(){
     hideElementById('home-screen')
     showElementById('play-ground')
+    continueGame()
 }
--- script/utility.js
+++ script/utility.js
@@ -7,3 +7,34 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden')
 }
+
+
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+
+
+
+
+
+
+// ------------------------------
+function getRandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+
+    // get a random index between 0-25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    // console.log(index);
+
+    const alphabet = alphabets[index];
+    // console.log(alphabet);
+    return alphabet
+}
